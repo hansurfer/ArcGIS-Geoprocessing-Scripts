4c12a3c5d752cc8fca0cd6b40587a11b872f3901
SanghongYoo <Sanghong.Yoo@dc.gov>
2019-01-28 10:36:33 -0500

    added numeric field support

--- scripts/arcgisgp_unique_field_values.py
+++ scripts/arcgisgp_unique_field_values.py
@@ -1,23 +1,52 @@
 # https://gis.stackexchange.com/questions/138263/how-do-i-count-occurrences-of-unique-field-values
 import arcpy
 
+def constructDict(table, fields):
+    """
+    construct a dictionary with table name and field values
+
+    Args:
+        table (str): table full path.
+        fields (str): field names, separated by ';'
+
+    Returns:
+        vDict (dict): a dictionary with table name key and field values
+    """
+    vDict = dict((fname, []) for fname in fields)
+    with arcpy.da.SearchCursor(table, fields) as cursor:
+        for row in cursor:
+            for i in range(len(row)):
+                vDict[fields[i]].append(row[i])
+    return vDict
+
+
+def uniquevalues(valueDict):
+    """
+    print unique value count
+
+    Args:
+        valueDict (dict): a dictionary with table name key and field values
+    """
+    for fname in valueDict:
+        arcpy.AddMessage("\nField: {}".format(fname))
+        arcpy.AddMessage("unique:")
+        occurances = valueDict[fname]
+        for i in sorted(set(occurances)):
+            icount = occurances.count(i)
+            if i is None:
+                arcpy.AddMessage("    None: 0")
+            elif isinstance(i, str):
+                arcpy.AddMessage("    {}: {}".format(i.encode('utf-8'), icount))
+            else:
+                arcpy.AddMessage("    {}: {}".format(i, icount))
+
+
 inputtable = arcpy.GetParameterAsText(0)
 fields = arcpy.GetParameterAsText(1)
 
-def uniquevalues(table, field):
-    with arcpy.da.SearchCursor(table, [field]) as cursor:
-        occurances = [row[0] for row in cursor]
-    for i in sorted(set(occurances)):
-        icount = occurances.count(i)
-        if i is None:
-            arcpy.AddMessage("    None: 0")
-        else:
-            arcpy.AddMessage("    {}: {}".format(i.encode('utf-8'), icount))
-
-
 fieldList = fields.split(";")
 
-for field in fieldList:
-    arcpy.AddMessage("\nField: {}".format(field))
-    arcpy.AddMessage("unique:")
-    uniquevalues(inputtable, field)
+numberrecords = int(arcpy.GetCount_management(inputtable).getOutput(0))
+uniquevalues(constructDict(inputtable, fieldList))
+
+arcpy.AddMessage("    ")
